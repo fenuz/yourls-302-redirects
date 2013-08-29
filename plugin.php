89f4a7c429599910ef7f5835603129f87575b78f
Frank Matheron <frankmatheron@gmail.com>
2013-08-29 16:28:55 +0200

    fix click logging for YOURLS 1.6

--- plugin.php
+++ plugin.php
@@ -11,5 +11,20 @@ Author URI: https://www.github.com/fenuz
 // use 302 redirects on shorturl keyword redirections (default yourls)
 yourls_add_action('redirect_shorturl', '_302_redirect_shorturl');
 function _302_redirect_shorturl($args) {
+    if (_302_redirect_log()) {
+        // Update click count in main table
+        yourls_update_clicks($args[1]);
+
+        // Update detailed log for stats
+        yourls_log_redirect($args[1]);
+    }
+    // do redirect
     yourls_redirect($args[0], 302);
+}
+
+// only log redirects on YOURLS 1.6 or higher, on lower versions 
+// yourls will have already logged the redirect.
+function _302_redirect_log() {
+    $version = explode('.', yourls_sanitize_version(YOURLS_VERSION));
+    return !empty($version) && ($version[0] > 1 || $version[1] >= 6);
 }
